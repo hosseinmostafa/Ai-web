5236234abf3aa6454677fd8d103e866f17bf3f88
hosseinmostafa <hesseinnmostafaa@gmail.com>
2024-11-26 23:13:44 +0200

    add all file footer and home and login and nav and products but only finsh nav and footer and stell work in home page now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Component/home/home.component';
+import { LoginComponent } from './Component/login/login.component';
+import { ProductsComponent } from './Component/products/products.component';
+import { CartComponent } from './Component/cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'cart', component: CartComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Component/home/home.component';
+import { LoginComponent } from './Component/login/login.component';
+import { NavComponent } from './Component/nav/nav.component';
+import { ProductsComponent } from './Component/products/products.component';
+import { CartComponent } from './Component/cart/cart.component';
+import { FooterComponent } from './Component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    NavComponent,
+    ProductsComponent,
+    CartComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
